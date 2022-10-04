34636641770765212d780eb99c357266213a041b
Noah <100643509+noneill256@users.noreply.github.com>
2022-10-04 12:04:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,8 @@ options = Options()
 options.headless = True # keep selenium from opening browser window
 driver = webdriver.Firefox(executable_path='PATH TO GECKODRIVER.EXE, options=options)
 
-# import the list of urls
-url_list = pd.read_excel('C:\users\1sled\desktop\state_list')
+# import the list of urls as a dataframe
+url_list = pd.read_excel('C:\users\1sled\desktop\state_list', sheet_name='Sheet1', header=0)
+
+for ind in url_list.index:
+		if (url_list['STATE'][ind] == 'California') and ('planet url_list['WEBSITE'][ind] 
