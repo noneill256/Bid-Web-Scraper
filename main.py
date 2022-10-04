e32b780127862498760b611d132ffb4db253a15f
Noah <100643509+noneill256@users.noreply.github.com>
2022-10-04 11:59:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
 
+
+# setting selenium options
+options = Options()
+options.headless = True # keep selenium from opening browser window
+driver = webdriver.Firefox(executable_path='PATH TO GECKODRIVER.EXE, options=options)
+
+# import the list of urls
+url_list = pd.read_excel('C:\users\1sled\desktop\state_list')
