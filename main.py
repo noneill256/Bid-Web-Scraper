61834acc8e47d415b6d2845b3a273d4341cb2b07
Noah <100643509+noneill256@users.noreply.github.com>
2022-10-05 12:41:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+# find how to get a whole column of data from openpyxl; alternatively, just use a column of it as a dataframe
+# find how to insert a whole array of data into an excel sheet
+
 import pandas as pd
 import numpy as pd
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
+from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter
 
 
 # setting selenium options
@@ -13,7 +18,14 @@ driver = webdriver.Firefox(executable_path='PATH TO GECKODRIVER.EXE, options=opt
 # import the list of urls as a dataframe
 url_list = pd.read_excel('C:\users\1sled\desktop\state_list', sheet_name='Sheet1', header=0)
 
+# import the workbook we'll be editing
+wb = load_workbook('C:\users\1sled\desktop\state_list')
+wb = wb['Sheet1']
+													
+# create empty lists we'll update
 num_bids_list = np.array([])
+old_num_bids_list = np.array([])
+													 
 # scroll through each url 
 for ind in url_list.index:
 	# if it's a CA state and a planetbid website
@@ -31,4 +43,5 @@ for ind in url_list.index:
 	
 	# the current number of bids
 	num_bids = element[1] # the number of bids
-	num_bids_list = np.append(num_bids_list, num_bids)														
+	num_bids_list = np.append(num_bids_list, num_bids)			
+																																			 
